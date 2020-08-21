c8bfdc20543f95f158bbe93ada1a7fa31dc3f8e3
Aakash Prabhu <aakprabhu@ucdavis.edu>
2020-08-21 13:55:41 -0700

    First unique character in a string

--- first_unique_char.cc
+++ first_unique_char.cc
@@ -4,33 +4,15 @@ public:
         if (s.empty()) 
             return -1;
         
-        std::unordered_map<char, int> ht;
         std::vector<int> v(26, 0);
         
-        ht.emplace(s[0], 0);
+        for (auto c : s)
+            v.at(c - 'a')++;
         
-        for (int i = 1; i < s.size(); i++) {
-            if (ht.find(s[i]) == ht.end() && v.at(s[i] - 'a') == 0)
-                // Haven't seen it before so it could be unique?
-                ht.emplace(s[i], i);
-            
-            else if (ht.find(s[i]) != ht.end()) {
-                // Already seen this character
-                ht.erase(s[i]);
-                v.at(s[i] - 'a') = 1;
-            }
-            
-            else
-                continue;
-        }
+        for (int i = 0; i < s.size(); i++)
+            if (v[s[i] - 'a'] == 1)
+                return i;
         
-        if (ht.empty())
-            return -1;
-        
-        int min_idx = std::numeric_limits<int>::max();
-        for (auto &e : ht)
-            min_idx = std::min(min_idx, e.second);
-        
-        return min_idx;
+        return -1;
     }
 };
